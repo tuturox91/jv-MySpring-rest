f3ccefde8144d245d73643e13d56fdde9c1dab98
maxpochepets <agreed_note@hotmail.com>
2021-06-09 18:32:47 +0300

    made salt array length constant

--- src/main/java/mate/academy/spring/util/HashUtil.java
+++ src/main/java/mate/academy/spring/util/HashUtil.java
@@ -6,6 +6,7 @@ import java.security.SecureRandom;
 
 public class HashUtil {
     public static final String HASH_ALGORITHM = "SHA-512";
+    public static final int CYPHER_INTENSITY = 16;
 
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPwd = new StringBuilder();
@@ -24,7 +25,7 @@ public class HashUtil {
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[CYPHER_INTENSITY];
         random.nextBytes(salt);
         return salt;
     }
